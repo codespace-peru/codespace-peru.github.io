51e2c266b817449ca598583c51f3194b548c1eb0
Carlos Montero <carlosa.monteroq@gmail.com>
2026-05-12 23:08:57 -0500

    Update project

--- sendemail.php
+++ sendemail.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	header('Content-type: application/json');
 	$status = array(
@@ -19,26 +18,4 @@
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
     echo json_encode($status);
-=======
-<?php
-	header('Content-type: application/json');
-	$status = array(
-		'type'=>'success',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
-	);
-
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
-
-    $email_from = $email;
-    $email_to = 'codespaceperu@gmail.com';//replace with your email
-
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
-
-    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
-
-    echo json_encode($status);
->>>>>>> df1a49aed8713a35489283919813bf63cdebe931
     die;
